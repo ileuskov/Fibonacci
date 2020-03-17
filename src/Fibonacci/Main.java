039d634c87c5b2eb25a9356392904c39723bc184
ileuskov <ileuskov@gmail.com>
2020-03-17 20:55:26 +0100

    Initial commit

---
+++ src/Fibonacci/Main.java
@@ -0,0 +1,54 @@
+package Fibonacci;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int count = 5;
+        fibonacci(5);
+        System.out.println("***");
+        int[] fibonacci_series = fibonacci_recursion(10);
+        for (int i = 0; i < fibonacci_series.length; i++){
+            System.out.print("" + fibonacci_series[i] + " | ");
+        }
+
+
+
+        // 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21
+    }
+
+    public static void fibonacci(int count){
+        int first = 0, second = 1;
+
+        System.out.println("First " + count + " Fibonacci numbers are: ");
+
+        for(int i = 0; i < count; i++){
+            System.out.print(first + " | ");
+           int temp = first + second;
+            first = second;
+            second = temp;
+        }
+    }
+
+    public static int[] fibonacci_recursion(int count) {
+        int number[] = new int[count];
+
+        for (int i = 0; i < count; i++){
+            number[i] = fibonacci_recursion_helper(i);
+        }
+        return number;
+    }
+
+    public static int fibonacci_recursion_helper(int count){
+        if (count == 0) {
+            return 0;
+        } else if (count == 1 || count == 2) {
+            return 1;
+        } else {
+            return fibonacci_recursion_helper(count - 1) + fibonacci_recursion_helper(count - 2);
+        }
+    }
+
+    // add a sum of n first fibonacci numbers
+}
+
+
